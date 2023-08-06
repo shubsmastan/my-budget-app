39a17f6ce1f32312880fbf501c4f1e820fdd3751
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-08-06 13:50:31 +0100

    Set up a test request

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. Views are request handlers in Django.
+
+def say_hello(req):
+    return HttpResponse("Hello world!")
