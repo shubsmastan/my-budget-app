5fb7a2aea116f3c857bfe3cacbe38b2fea5d2123
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-08-08 22:22:41 +0100

    Change build command for Render deploy

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
-pip install -r requirements.txt
+pip install --upgrade pip && pip install -r requirements.txt
 
 # python manage.py collectstatic --no-input
 python manage.py migrate
