fae70cc5402f6343c2e99d9473380d56c080c9d5
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-08-08 12:51:38 +0100

    Add exception to catch unauthentiated users

--- server/category/views.py
+++ server/category/views.py
@@ -8,21 +8,25 @@ JWTAuthenticator = JWTAuthentication()
 
 
 def get_categories(req):
-    # if not req.headers["Authorization"]:
-    #     return JsonResponse({"error": "Need to be an authenticated user."})
-    token_obj = JWTAuthenticator.authenticate(req)
-    username = str(token_obj[0])
-    user = User.objects.get(username=username)
-    user_id = user.id
-    if req.method == "GET":
-        q = Category.objects.filter(user_id=user_id)
-        return JsonResponse([cat.serialise() for cat in q], safe=False)
-    if req.method == "POST":
-        name = req.POST.get("name")
-        if name == None or name == "":
-            return JsonResponse({"error": "Please enter a category name."}, status=400)
-        category = Category(name=name, user_id=user_id)
-        category.save()
-        return JsonResponse({"categories": []})
-    else:
-        return JsonResponse({"error": "Method not allowed."})
+    try:
+        token_obj = JWTAuthenticator.authenticate(req)
+        username = str(token_obj[0])
+        user = User.objects.get(username=username)
+        user_id = user.id
+        if req.method == "GET":
+            q = Category.objects.filter(user_id=user_id)
+            return JsonResponse([cat.serialise() for cat in q], safe=False)
+        elif req.method == "POST":
+            name = req.POST.get("name")
+            if name == None or name == "":
+                return JsonResponse(
+                    {"error": "Please enter a category name."}, status=400
+                )
+            category = Category(name=name, user_id=user_id)
+            category.save()
+            q = Category.objects.filter(user_id=user_id)
+            return JsonResponse([cat.serialise() for cat in q], safe=False)
+        else:
+            return JsonResponse({"error": "Method not allowed."}, status=400)
+    except:
+        return JsonResponse({"error": "Authentication failed."}, status=401)
