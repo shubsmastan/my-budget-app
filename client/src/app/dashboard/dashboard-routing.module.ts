e01815e0175045e0e296328e272404e34dc0cf68
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-08-08 18:45:54 +0100

    Small style changes and code clean up

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { CategoryComponent } from './category/category.component';
 import { authGuard } from '../auth/auth.guard';
 
 const dashboardRoutes: Routes = [
   {
     path: 'dashboard',
-    component: MainComponent,
     canActivate: [authGuard],
-  },
-  {
-    path: 'dashboard/:id',
-    component: CategoryComponent,
-    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full',
+      },
+      {
+        path: ':id',
+        component: MainComponent,
+      },
+    ],
   },
 ];
 
